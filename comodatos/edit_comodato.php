3c23fe0258277f5a5f33cadd216aa8a3ac068ee8
Micaelgreff <micaelvasques@yahoo.com.br>
2022-07-29 07:34:34 -0300

    ajuste02

--- comodatos/edit_comodato.php
+++ comodatos/edit_comodato.php
@@ -1,5 +1,6 @@
 <?php
 include('concomodato.php'); // Arquivo contendo a variável com os dados da conexão ao banco de dados
+include('verificalogin.php');
 if(isset($_POST['submit']))
 {
     // Armazenando os dados recebidos do formulário através do método POST
@@ -11,14 +12,20 @@ if(isset($_POST['submit']))
     $patrimonio = $_POST["patrimonio"];
     $id = $_POST["id"];
 
+    $data = date_create($data); //Pega a string e transforma em data
+    $data = date_format($data, 'd/m/Y'); //converte o formato da data para DD/MM/YYYY
     $query = "UPDATE comodatos SET nome = '$nome', cpf = '$cpf', data = '$data', carteira = '$setor', equipamento = '$equipamento', pat = '$patrimonio' WHERE id = '$id'";
 
     if(mysqli_query($concomodato, $query))
     {
         echo "DADOS ATUALIZADOS!";
+        header('refresh:1.5; url=comodato.php');
     }
     else{
         echo "Error: " . $query . ":-" . mysqli_error($concomodato);
+        echo <br>
+        echo "Favor contatar o administrador!";
+        header('refresh:5; url=comodato.php');
     }
 
     mysqli_close($concomodato); // Encerrando a conexão com o banco de dados
